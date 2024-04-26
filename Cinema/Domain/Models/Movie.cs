8a0335f4eeac890b9b2d0c5a1e5bdea8713dc16a
Googloolvoov1377 <tem4ik20.1@gmail.com>
2024-04-26 18:41:28 +0300

    create class for DB

---
+++ Cinema/Domain/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public string genre { get; set; }
+        public string description { get; set; }
+        public int duration { get; set; }
+        public int ageRating { get; set; }
+        public string rating { get; set; }
+        public DateTime releaseDate { get; set; }
+        public string trailers { get; set; }
+    }
+}
