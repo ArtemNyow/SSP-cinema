d7db3384afcae873e1259c2404b9a06f233ce292
Artem Lykhatskyi <tem4ik20.1@gmail.com>
2024-05-01 18:36:23 +0300

    Complete

---
+++ Cinema/WebApi/MovieController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class MovieController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Movie>> GetMovies()
+        {
+
+        }
+
+    }
+}
