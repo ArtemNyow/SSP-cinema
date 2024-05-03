95264d8b47ea90bbe2bc9d57beef5930c74d89cb
Anton <anton260903@gmail.com>
2024-05-03 17:45:31 +0300

    Service interfaces added

---
+++ Cinema/BLL/Interfaces/ICrud.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Models;
+
+namespace BLL.Interfaces
+{
+    public interface ICrud<T> where T : BaseEntity
+    {
+        IQueryable<T> GetAll();
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T model);
+        Task UpdateAsync(T model);
+        Task DeleteAsync(int id);
+    }
+}
