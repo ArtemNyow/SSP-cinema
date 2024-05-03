ed1e3df88f164f72d66d92f805983ec6b4017e33
Anton <anton260903@gmail.com>
2024-05-03 22:03:10 +0300

    Dependency injection added

---
+++ Cinema/BLL/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using BLL.Interfaces;
+using BLL.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BLL
+{
+    public static class DependencyInjection
+    {
+        public static void AddBusinessLogicLayer(this IServiceCollection services)
+        {
+            services.AddScoped<IActorService, ActorService>();
+            services.AddScoped<IDirectorService, DirectorService>();
+            services.AddScoped<IGenreService, GenreService>();
+            services.AddScoped<IHallService, HallService>();
+            services.AddScoped<IMovieService, MovieService>();
+            services.AddScoped<IPersonService, PersonService>();
+            services.AddScoped<ISessionService, SessionService>();
+            services.AddScoped<ITicketService, TicketService>();
+            services.AddScoped<IUserService, UserService>();
+
+            services.AddSingleton<IPasswordHashService, PasswordHashService>();
+        }
+    }
+}
