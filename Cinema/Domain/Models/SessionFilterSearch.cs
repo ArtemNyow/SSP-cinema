5ad398f2d03d35a64734c47a1c87f185996ec7f7
Anton <anton260903@gmail.com>
2024-05-07 23:30:05 +0300

    SessionFilterSearch model added

---
+++ Cinema/Domain/Models/SessionFilterSearch.cs
@@ -0,0 +1,39 @@
+﻿namespace Domain.Models
+{
+    public class SessionFilterSearch
+    {
+        public TimeOnly? TimeFrom { get; set; }
+        public TimeOnly? TimeTo { get; set; }     
+        public DateOnly? DateFrom { get; set; }
+        public DateOnly? DateTo { get; set; }
+        public int? MinPrice { get; set; }
+        public int? MaxPrice { get; set; }
+        public int? HallNumber { get; set; }
+        public string? MovieGenre { get; set; }
+        public string? MovieTitle { get; set; }
+
+        public SessionFilterSearch() { }
+
+        public SessionFilterSearch(
+            TimeOnly? timeFrom, 
+            TimeOnly? timeTo, 
+            DateOnly? dateFrom, 
+            DateOnly? dateTo, 
+            int? minPrice, 
+            int? maxPrice, 
+            int hallNumber, 
+            string movieGenre, 
+            string movieTitle)
+        {
+            TimeFrom = timeFrom;
+            TimeTo = timeTo;
+            DateFrom = dateFrom;
+            DateTo = dateTo;
+            MinPrice = minPrice;
+            MaxPrice = maxPrice;
+            HallNumber = hallNumber;
+            MovieGenre = movieGenre;
+            MovieTitle = movieTitle;
+        }
+    }
+}
