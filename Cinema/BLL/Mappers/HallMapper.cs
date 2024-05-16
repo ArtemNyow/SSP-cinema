29aeb7d8bf8f5c48205b58586aef829d7c999571
Anton <anton260903@gmail.com>
2024-05-17 00:11:07 +0300

    Mappers added for DTOs

---
+++ Cinema/BLL/Mappers/HallMapper.cs
@@ -0,0 +1,34 @@
+﻿using BLL.DTOs;
+using Domain.Models;
+
+namespace BLL.Mappers
+{
+    public static class HallMapper
+    {
+        public static HallDto ToDto(this Hall hall)
+        {
+            return new HallDto
+            {
+                ID = hall.ID,
+                Number = hall.Number,
+                RowsCount = hall.RowsCount,
+                RowsVipCount = hall.RowsVipCount,
+                SeatsCountPerRow = hall.SeatsCountPerRow,
+                SeatsVipCountPerRow = hall.SeatsVipCountPerRow,
+            };
+        }
+
+        public static Hall ToEntity(this HallDto hall)
+        {
+            return new Hall
+            {
+                ID = hall.ID,
+                Number = hall.Number,
+                RowsCount = hall.RowsCount,
+                RowsVipCount = hall.RowsVipCount,
+                SeatsCountPerRow = hall.SeatsCountPerRow,
+                SeatsVipCountPerRow = hall.SeatsVipCountPerRow,
+            };
+        }
+    }
+}
---
+++ Cinema/BLL/Mappers/TicketMapper.cs
@@ -0,0 +1,34 @@
+﻿using BLL.DTOs;
+using Domain.Models;
+
+namespace BLL.Mappers
+{
+    public static class TicketMapper
+    {
+        public static TicketDto ToDto(this Ticket ticket)
+        {
+            return new TicketDto
+            {
+                ID = ticket.ID,
+                UserID = ticket.UserID,
+                SessionID = ticket.SessionID,
+                Price = ticket.Price,
+                RowNumber = ticket.RowNumber,
+                SeatNumber = ticket.SeatNumber,
+            };
+        }
+
+        public static Ticket ToEntity(this TicketDto ticket)
+        {
+            return new Ticket
+            {
+                ID = ticket.ID,
+                UserID = ticket.UserID,
+                SessionID = ticket.SessionID,
+                Price = ticket.Price,
+                RowNumber = ticket.RowNumber,
+                SeatNumber = ticket.SeatNumber,
+            };
+        }
+    }
+}
